c911fde2ebf1253e1f394647fa17660616649b36
Sarfaraz Fida <148467563+sarfaraz-baloch@users.noreply.github.com>
2024-02-28 09:42:29 +0500

    array and method

---
+++ arrays/arrays.js
@@ -0,0 +1,45 @@
+// fruits names
+let fruits = ["apple" , "banana", "orange", "grapes"]
+// console.log(fruits)
+
+// removeing last index of fruits
+fruits.pop()
+// console.log(fruits)
+
+// adding frist index of fruits
+fruits.push("mango")
+console.log(fruits)
+
+
+// adding and removeing frist index number
+// shift remove
+// unshift add
+let user = fruits
+user.shift()
+user.unshift("pineapple" , "stobarey", "watermelon")
+console.log(user)
+
+// 
+
+let numbers = [1 ,2 , 3 , 4, 5, 6, 7, 8, 9, 10]
+
+numbers.splice(0,10 ,11, 12, 13,14,32)
+
+let result = numbers.slice(numbers)
+
+let result1 =   result.length
+
+
+// console.log(result2)
+
+
+
+
+
+
+
+
+
+
+
+
