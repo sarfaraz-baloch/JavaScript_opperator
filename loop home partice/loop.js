7709af3b790c00d775c62610954be180c6f17194
Sarfaraz Fida <148467563+sarfaraz-baloch@users.noreply.github.com>
2024-02-29 13:54:23 +0500

    for Loops

---
+++ loop home partice/loop.js
@@ -0,0 +1,30 @@
+// basic loops
+
+for(let i = 1; i <= 5; i++){
+    console.log(i);
+}
+
+//reverse counting
+
+for(let i = 10; i >=1; i--){
+    console.log(i)
+}
+
+// table of 2 
+for(let i = 1; i <=10; i++){
+
+    console.log(`2 x ${i} = ${2 * i}`)
+}
+
+// drwaing 
+
+let pattern = `**********`
+for (let i = 1 ; i <= 10 ; i++){
+   pattern = pattern.slice(1)
+    console.log(pattern)
+}
+
+// summary for loop
+
+// for loop is use for repeatsion writing code one by one  
+// for loop consume time 
